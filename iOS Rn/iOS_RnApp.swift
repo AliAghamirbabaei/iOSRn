00f08e10171781bdaf72b4036f8d2995081e0603
Ali Aghamirbabaei <Me@AliAghamirbabaei.com>
2022-12-10 01:41:15 +0330

    Initial Commit

---
+++ iOS Rn/iOS_RnApp.swift
@@ -0,0 +1,17 @@
+//
+//  iOS_RnApp.swift
+//  iOS Rn
+//
+//  Created by Ali Aghamirbabaei on 12/10/22.
+//
+
+import SwiftUI
+
+@main
+struct iOS_RnApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
